16acac786b7fd5ea0ec1d6beb08d96ecdf0b620a
David Kinder <david.kinder@david.kinder>
2011-10-15 08:52:11 +0100

    Update distribution creating batch file

--- MakeDist.bat
+++ MakeDist.bat
@@ -1,53 +1,53 @@
 @echo off
 
-"\Program Files\Zip\zip" \Temp\WindowsGlk.zip Glk.c WinGlk.html
-"\Program Files\Zip\zip" -j \Temp\WindowsGlk.zip Executables\Release\Glk*.* Executables\Release\ScaleGfx.dll
-"\Program Files\Zip\zip" -j \Temp\WindowsGlk.zip GlkDll\Glk.def
-"\Program Files\Zip\zip" -r \Temp\WindowsGlk.zip Examples\* Include\*
+"%ProgramFiles(x86)%\Zip\zip" \Temp\WindowsGlk.zip Glk.c WinGlk.html
+"%ProgramFiles(x86)%\Zip\zip" -j \Temp\WindowsGlk.zip Executables\Release\Glk*.* Executables\Release\ScaleGfx.dll
+"%ProgramFiles(x86)%\Zip\zip" -j \Temp\WindowsGlk.zip GlkDll\Glk.def
+"%ProgramFiles(x86)%\Zip\zip" -r \Temp\WindowsGlk.zip Examples\* Include\*
 
-"\Program Files\Zip\zip" \Temp\WindowsGlkSrc.zip Glk.c WinGlk.html MakeDist.bat
-"\Program Files\Zip\zip" -r \Temp\WindowsGlkSrc.zip GlkDll\* Include\*
+"%ProgramFiles(x86)%\Zip\zip" \Temp\WindowsGlkSrc.zip Glk.c WinGlk.html MakeDist.bat
+"%ProgramFiles(x86)%\Zip\zip" -r \Temp\WindowsGlkSrc.zip GlkDll\* Include\*
 pushd \Programs
-"\Program Files\Zip\zip" \Temp\WindowsGlkSrc.zip Libraries\libmodplug\*
-"\Program Files\Zip\zip" \Temp\WindowsGlkSrc.zip Libraries\mfc\*
-"\Program Files\Zip\zip" \Temp\WindowsGlkSrc.zip Libraries\ScaleGfx\*.h
-"\Program Files\Zip\zip" \Temp\WindowsGlkSrc.zip Libraries\ScaleGfx\*.cpp
-"\Program Files\Zip\zip" \Temp\WindowsGlkSrc.zip Libraries\ScaleGfx\*.def
-"\Program Files\Zip\zip" \Temp\WindowsGlkSrc.zip Libraries\ScaleGfx\Makefile
+"%ProgramFiles(x86)%\Zip\zip" \Temp\WindowsGlkSrc.zip Libraries\libmodplug\*
+"%ProgramFiles(x86)%\Zip\zip" \Temp\WindowsGlkSrc.zip Libraries\mfc\*
+"%ProgramFiles(x86)%\Zip\zip" \Temp\WindowsGlkSrc.zip Libraries\ScaleGfx\*.h
+"%ProgramFiles(x86)%\Zip\zip" \Temp\WindowsGlkSrc.zip Libraries\ScaleGfx\*.cpp
+"%ProgramFiles(x86)%\Zip\zip" \Temp\WindowsGlkSrc.zip Libraries\ScaleGfx\*.def
+"%ProgramFiles(x86)%\Zip\zip" \Temp\WindowsGlkSrc.zip Libraries\ScaleGfx\Makefile
 popd
 
-"\Program Files\Zip\zip" -j \Temp\WinGlulxe.zip Executables\Release\Glulxe.*
-"\Program Files\Zip\zip" -j \Temp\WinGlulxe.zip Executables\Release\Glk*.dll
-"\Program Files\Zip\zip" -j \Temp\WinGlulxe.zip Executables\Release\ScaleGfx.dll
+"%ProgramFiles(x86)%\Zip\zip" -j \Temp\WinGlulxe.zip Executables\Release\Glulxe.*
+"%ProgramFiles(x86)%\Zip\zip" -j \Temp\WinGlulxe.zip Executables\Release\Glk*.dll
+"%ProgramFiles(x86)%\Zip\zip" -j \Temp\WinGlulxe.zip Executables\Release\ScaleGfx.dll
 
-"\Program Files\Zip\zip" -r \Temp\WinGlulxeSrc.zip Glulxe\help\* Glulxe\res\* Glulxe\msvc\*
-"\Program Files\Zip\zip" -r \Temp\WinGlulxeSrc.zip Installer\*
-"\Program Files\Zip\zip" \Temp\WinGlulxeSrc.zip Glulxe\resource.h
-"\Program Files\Zip\zip" \Temp\WinGlulxeSrc.zip Glulxe\osdepend.c
-"\Program Files\Zip\zip" \Temp\WinGlulxeSrc.zip Glulxe\winstart.c
-"\Program Files\Zip\zip" \Temp\WinGlulxeSrc.zip Glulxe\Glulxe.rc
-"\Program Files\Zip\zip" \Temp\WinGlulxeSrc.zip Glulxe\Makefile.win
+"%ProgramFiles(x86)%\Zip\zip" -r \Temp\WinGlulxeSrc.zip Glulxe\help\* Glulxe\res\* Glulxe\msvc\*
+"%ProgramFiles(x86)%\Zip\zip" -r \Temp\WinGlulxeSrc.zip Installer\*
+"%ProgramFiles(x86)%\Zip\zip" \Temp\WinGlulxeSrc.zip Glulxe\resource.h
+"%ProgramFiles(x86)%\Zip\zip" \Temp\WinGlulxeSrc.zip Glulxe\osdepend.c
+"%ProgramFiles(x86)%\Zip\zip" \Temp\WinGlulxeSrc.zip Glulxe\winstart.c
+"%ProgramFiles(x86)%\Zip\zip" \Temp\WinGlulxeSrc.zip Glulxe\Glulxe.rc
+"%ProgramFiles(x86)%\Zip\zip" \Temp\WinGlulxeSrc.zip Glulxe\Makefile.win
 
-"\Program Files\Zip\zip" -j \Temp\wingit.zip Executables\Release\Git.exe
-"\Program Files\Zip\zip" -j \Temp\wingit.zip Executables\Release\Git.chm
-"\Program Files\Zip\zip" -j \Temp\wingit.zip Executables\Release\Glk*.dll
-"\Program Files\Zip\zip" -j \Temp\wingit.zip Executables\Release\ScaleGfx.dll
-"\Program Files\Zip\zip" -j \Temp\wingit.zip Git\README.txt
+"%ProgramFiles(x86)%\Zip\zip" -j \Temp\wingit.zip Executables\Release\Git.exe
+"%ProgramFiles(x86)%\Zip\zip" -j \Temp\wingit.zip Executables\Release\Git.chm
+"%ProgramFiles(x86)%\Zip\zip" -j \Temp\wingit.zip Executables\Release\Glk*.dll
+"%ProgramFiles(x86)%\Zip\zip" -j \Temp\wingit.zip Executables\Release\ScaleGfx.dll
+"%ProgramFiles(x86)%\Zip\zip" -j \Temp\wingit.zip Git\README.txt
 
-"\Program Files\Zip\zip" \Temp\wingit-src.zip Git\git_windows.c
-"\Program Files\Zip\zip" \Temp\wingit-src.zip Git\Makefile.win
-"\Program Files\Zip\zip" -r \Temp\wingit-src.zip Git\win\* Git\help\*
+"%ProgramFiles(x86)%\Zip\zip" \Temp\wingit-src.zip Git\git_windows.c
+"%ProgramFiles(x86)%\Zip\zip" \Temp\wingit-src.zip Git\Makefile.win
+"%ProgramFiles(x86)%\Zip\zip" -r \Temp\wingit-src.zip Git\win\* Git\help\*
 
-"\Program Files\Zip\zip" -j \Temp\scare_win.zip Executables\Release\Scare.exe
-"\Program Files\Zip\zip" -j \Temp\scare_win.zip Executables\Release\Glk*.dll
-"\Program Files\Zip\zip" -j \Temp\scare_win.zip Executables\Release\ScaleGfx.dll
-"\Program Files\Zip\zip" -j \Temp\scare_win.zip Scare\README Scare\ChangeLog
-"\Program Files\Zip\zip" -j \Temp\scare_win.zip Scare\COPYING Scare\doc\RUNNING
+"%ProgramFiles(x86)%\Zip\zip" -j \Temp\scare_win.zip Executables\Release\Scare.exe
+"%ProgramFiles(x86)%\Zip\zip" -j \Temp\scare_win.zip Executables\Release\Glk*.dll
+"%ProgramFiles(x86)%\Zip\zip" -j \Temp\scare_win.zip Executables\Release\ScaleGfx.dll
+"%ProgramFiles(x86)%\Zip\zip" -j \Temp\scare_win.zip Scare\README Scare\ChangeLog
+"%ProgramFiles(x86)%\Zip\zip" -j \Temp\scare_win.zip Scare\COPYING Scare\doc\RUNNING
 pushd Scare
-"\Program Files\Zip\zip" -r \Temp\scare_win.zip win32\*
+"%ProgramFiles(x86)%\Zip\zip" -r \Temp\scare_win.zip win32\*
 popd
 
 pushd Installer
-"\Program Files\NSIS\makensis" Glulxe.nsi
+"%ProgramFiles(x86)%\NSIS\makensis" Glulxe.nsi
 move *.exe \Temp
 popd
